8412361f17387e9e9cedf7a7f83b91fcbe7973da
Thomas Maerz <7740810+thomasmaerz@users.noreply.github.com>
2026-05-01 14:19:53 -0700

    Final fixes for virt-manager, virt-viewer, libvirt, and gtk-vnc
    - Update libvirt to 12.2.0 with Apple Silicon patch
    - Update gtk-vnc to 1.5.0
    - Fix virt-manager 5.1.0 meson build with virtualenv for Python deps
    - Fix virt-viewer 11.0 meson build and linkage deps

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -1,8 +1,8 @@
 class Libvirt < Formula
   desc "C virtualization API"
   homepage "https://libvirt.org/"
-  url "https://libvirt.org/sources/libvirt-7.10.0.tar.xz"
-  sha256 "cb318014af097327928c6e3d72922e3be02a3e6401247b2aa52d9ab8e0b480f9"
+  url "https://libvirt.org/sources/libvirt-12.2.0.tar.xz"
+  sha256 "ac93cd0da743a6c231911fb549399b415102ecfee775329bebbf61ed843b9786"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
   head "https://gitlab.com/Menci/libvirt.git", branch: "v7.10.0-apple-silicon"
 
@@ -17,17 +17,19 @@ class Libvirt < Formula
   depends_on "gnu-sed"
   depends_on "gnutls"
   depends_on "grep"
+  depends_on "json-c"
   depends_on "libgcrypt"
   depends_on "libiscsi"
   depends_on "libssh2"
+  depends_on "readline"
   depends_on "yajl"
 
   uses_from_macos "curl"
   uses_from_macos "libxslt"
 
   def install
-    # Apply apple silicon patch if head
-    inreplace "src/qemu/qemu_command.c", "qemuBuildAccelCommandLine(cmd, def);", "" if build.head?
+    # Apply apple silicon patch
+    inreplace "src/qemu/qemu_command.c", "qemuBuildAccelCommandLine(cmd, def);", ""
 
     mkdir "build" do
       args = %W[
--- Formula/virt-manager.rb
+++ Formula/virt-manager.rb
@@ -1,4 +1,6 @@
 class VirtManager < Formula
+  include Language::Python::Virtualenv
+
   desc "App for managing virtual machines"
   homepage "https://virt-manager.org/"
   url "https://releases.pagure.org/virt-manager/virt-manager-5.1.0.tar.xz"
@@ -10,7 +12,6 @@ class VirtManager < Formula
   depends_on "ninja" => :build
 
   depends_on "adwaita-icon-theme"
-  depends_on "glib"
   depends_on "gtk+3"
   depends_on "gtk-vnc"
   depends_on "gtksourceview4"
@@ -23,10 +24,56 @@ class VirtManager < Formula
   depends_on "spice-gtk"
   depends_on "vte3"
 
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
+    sha256 "2bbf76fd432960138b3ef6dda3dde0544f27cbf8546c458e60baf371917ba9ee"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/eb/7f/a6c278746ddbd7094b019b08d1b2187101b1f596f35f81dc27f57d8fcf7c/charset-normalizer-2.0.6.tar.gz"
+    sha256 "5ec46d183433dcbd0ab716f2d7f29d8dee50505b3fdb40c6b985c7c4f5a3591f"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/cb/38/4c4d00ddfa48abe616d7e572e02a04273603db446975ab46bbcd36552005/idna-3.2.tar.gz"
+    sha256 "467fbad99067910785144ce333826c71fb0e63a425657295239737f7ecd125f3"
+  end
+
+  resource "libvirt-python" do
+    url "https://libvirt.org/sources/python/libvirt-python-12.2.0.tar.gz"
+    sha256 "80c3fe2ae1062abf56456f52518bd670f9ec3917b7f85e152b347ac6b6faf880"
+  end
+
+  resource "pycairo" do
+    url "https://files.pythonhosted.org/packages/bc/3f/64e6e066d163fbcf13213f9eeda0fc83376243335ea46a66cefd70d62e8f/pycairo-1.20.1.tar.gz"
+    sha256 "1ee72b035b21a475e1ed648e26541b04e5d7e753d75ca79de8c583b25785531b"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/e7/01/3569e0b535fb2e4a6c384bdbed00c55b9d78b5084e0fb7f4d0bf523d7670/requests-2.26.0.tar.gz"
+    sha256 "b8aa58f8cf793ffd8782d3d8cb19e66ef36f7aba4353eec859e74678b01b07a7"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/80/be/3ee43b6c5757cabea19e75b8f46eaf05a2f5144107d7db48c7cf3a864f73/urllib3-1.26.7.tar.gz"
+    sha256 "4987c65554f7a2dbf30c18fd48778ef124af6fab771a377103da0585e2336ece"
+  end
+
   def install
-    system "meson", "setup", "build", *std_meson_args, "-Dcompile-schemas=false", "-Dupdate-icon-cache=false"
+    venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install resources
+
+    system "meson", "setup", "build", *std_meson_args,
+           "-Dcompile-schemas=false", "-Dupdate-icon-cache=false"
     system "meson", "compile", "-C", "build"
     system "meson", "install", "-C", "build"
+
+    # Patch wrapper scripts to use the virtual env's Python
+    bin.children.each do |f|
+      next unless f.file?
+
+      inreplace f, "#!/usr/bin/env python3", "#!#{libexec}/bin/python3"
+    end
   end
 
   def post_install
